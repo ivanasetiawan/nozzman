3e3449429980d4659d9ee03df36ff73962495176
Ivana Setiawan <ivana@mangrove.nl>
2012-11-15 16:32:50 +0100

    Styling and content

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 	keys();
+	//parallax();
 });
 
 var keys = function(){
@@ -7,3 +8,17 @@ var keys = function(){
 		$('.pirate').toggleClass('move');
 	});
 }
+
+var parallax = function(){
+	$('div[data-type="background"]').each(function(){
+		var $bgobj = $(this); // assigning the object
+		$(window).scroll(function() {
+			var yPos = -($window.scrollTop() / $bgobj.data('speed'));
+			// Put together our final background position
+			var coords = '50% '+ yPos + 'px';
+			// Move the background
+			$bgobj.css({ backgroundPosition: coords });
+		});
+	});
+}
+
